0e0c4fb6150c9c4290eddbf987a0da61ebd72faf
its-robii <robiulhassanrobi33@gmail.com>
2025-05-22 20:08:21 +0600

    my portfoloio

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,9 @@ import Experiences from "./sections/Experiences";
 import Footer from "./sections/Footer";
 import Hero from "./sections/Hero";
 import Navbar from "./sections/Navbar";
+
 import Projects from "./sections/Projects";
-import Testimonial from "./sections/Testimonial";
+
 
 
 function App() {
